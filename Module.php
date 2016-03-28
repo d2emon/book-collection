5774c6f6bda9812fa8a917521cc04ef7c1c0ecd8
Dmitry Kutsenko <d2emonium@gmail.com>
2016-03-29 00:47:30 +0300

    Gii generated

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace d2emon\books;
+
+/**
+ * books module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'd2emon\books\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
